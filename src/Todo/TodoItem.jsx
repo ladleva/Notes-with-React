9112e5e9d95202b9b328207f4d2f10264c97fecc
ladleva <demihova-lera@rambler.ru>
2022-04-27 21:28:24 +0300

    new commit

---
+++ src/Todo/TodoItem.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+import Context from '../context'
+
+function TodoItem({ todo, index, onChange }) {
+  const { removeTodo } = useContext(Context)
+  const classes = []
+
+  if (todo.completed) {
+    classes.push('done')
+  }
+
+  return (
+    <li className="item">
+      <span className={classes.join(' ')}>
+        <input
+          className='input'
+          type='checkbox'
+          checked={todo.completed}
+          onChange={() => onChange(todo.id)}
+        />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+
+      <button className='rm' onClick={removeTodo.bind(null, todo.id)}>
+        &times;
+      </button>
+    </li>
+  )
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+}
+
+export default TodoItem;
